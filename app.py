0b741e1360462dc19a619c73b8c3f29df643ccf7
unknown <m.vasilkin@gmail.com>
2014-10-22 21:37:01 -0400

    Really messy SQL stuff

--- app.py
+++ app.py
@@ -26,23 +26,28 @@ app = Flask(__name__)
 def home():
         dic = {"key1":"value1","key2":"value2","key3":"value3"}
         #dic is just for testing so yeah
-    if request.method == "GET":
-        return render_template("index.html",dic=dic,postBoolean = False)
-    else: #post
-        postTitle = request.args.get("title")
-        postText = request.args.get("posttxt")
-        q = "insert into posts values(" + postTitle + ", " + postText + ", 0)"
-        c.execute(q)
-        conn.commit()
-        return render_template("index.html",postBoolean = True)
+        if request.method == "GET":
+                return render_template("index.html",dic=dic,postBoolean = False)
+        else: #post
+                postTitle = request.args.get("title")
+                postText = request.args.get("posttxt")
+                q = "insert into posts values(" + postTitle + ", " + postText + ", 0)"
+                c.execute(q)
+                conn.commit()
+                return render_template("index.html",postBoolean = True)
     
     
 @app.route("/<title>",methods=["POST", "GET"])
 def blogPage():
-    if request.method == "GET":
-        return render_template("post.html",title=title)
-    else:#I think this should be different if you just added a comment
-        return render_template("post.hmtl",title=title)
+        if request.method == "GET":
+                return render_template("post.html",title=title)
+        else:#I think this should be different if you just added a comment
+                comment = request.args.get("comment")
+                id = request.args.get("post.id") #this may be way off
+                q = "insert into posts values(" + comment + ", " + post.id + ", 0)"
+                c.execute(q)
+                conn.commit()
+                return render_template("post.hmtl",title=title)
 
 if __name__ == "__main__":
     app.debug=True
