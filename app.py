782220ba85da55eeaee20ab46a79196f1fa13f52
deannataylor <dmtaylor@gmail.com>
2014-10-22 23:36:03 -0400

    fixing submit button

--- app.py
+++ app.py
@@ -27,24 +27,37 @@ for row in result:
         print row
 
 thing = []
-for row in result:
-        thing.append(row)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
         #dic is just for testing so yeah
         if request.method == "GET":
+                print thing
+                if len(thing)==0:
+                        print "nope"
                 return render_template("index.html",thing=thing, postBoolean = False)
         else: #post
                 postTitle = request.args.get("title")
-                postText = request.args.get("posttxt")
+                postText = request.args.get("post")
                 i=0
                 q = "insert into posts values(" + postTitle + ", " + postText + ", i)"
                 i=i+1
                 c.execute(q)
                 conn.commit()
+                print thing
+                if len(thing) == 0:
+                        print "nope"
                 return render_template("index.html",postBoolean = True)
 
+@app.route("/run", methods=["POST", "GET"])
+def run():
+        i=0
+        "INSERT into posts values(" + postTitle + "," + postText + "i)"
+        i=i+1
+        for row in result:
+                thing.append(row)
+                print thing
+        return render_template("index.html", postBoolean=true)
     
 @app.route("/<title>",methods=["POST", "GET"])
 def blogPage():
